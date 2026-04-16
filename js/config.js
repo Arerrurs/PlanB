fa51bbc37b3ec2b7327e37a2b2da8be36927bf3a
Arerrurs <wiedererstehen@gmail.com>
2026-04-16 11:54:28 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = 'https://YOUR_PROJECT.supabase.co';
-export const SUPABASE_ANON_KEY = 'YOUR_PUBLISHABLE_KEY';
+export const SUPABASE_URL = 'https://gxukhvqnjjmrudzshjpx.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_lIUmGAmhGfZt_XCKywHxKg_b-njIfSO';
